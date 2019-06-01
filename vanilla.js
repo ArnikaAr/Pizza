8c56b02014bf5e5281488aeed757279ee7fc4217
Arina <alihoo.al@gmail.com>
2019-06-01 20:17:34 +0300

    3

--- vanilla.js
+++ vanilla.js
@@ -1,27 +1,30 @@
 const pizzas = [
-  { name: 'Karbonara', price: 10, kkal: 400, list: { cheese: 'dorBlue', meeeet: 'peperoni', meet: 'peperoni', vegetables: 'tomato', souse: 'tomatoSouse' }, img: './img/pizza2.jpg' },
-  { name: 'Margarita', price: 40, kkal: 390, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'tomatoSouse1', toping: 'chili' }, img: './img/pizza1.jpg' },
-  { name: '4Cheese', price: 12, kkal: 390, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'tomatoSouse2' }, img: './img/pizza3.jpg' },
-  { name: 'Tropicano', kkal: 390, price: 18, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'tomatoSouse4' }, img: './img/pizza4.jpg' },
-  { name: '4sizon', price: 26, kkal: 390, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'tomatoSouse6' }, img: './img/pizza1.jpg' },
-  { name: '4meats', price: 16, kkal: 390, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'chilli' }, img: './img/pizza2.jpg' },
-  { name: 'Peperoni', price: 5, kkal: 390, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'garlick' }, img: './img/pizza3.jpg' },
-  { name: 'LaGalina', kkal: 400, price: 18, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'sweet' }, img: './img/pizza4.jpg' },
+  { name: 'Karbonara', price: 15, kkal: 400, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'tomatoSouse' }, img: './img/pizza2.jpg' },
+  { name: 'Margarita', price: 23, kkal: 390, list: { cheese: 'parmezan', meet: 'ham', vegetables: 'tomato', souse: 'tomatoSouse1', toping: 'chili' }, img: './img/pizza1.jpg' },
+  { name: '4Cheese', price: 16, kkal: 390, list: { cheese: 'Mozzarella', meet: 'peperoni', vegetables: 'tomato', souse: 'tomatoSouse2' }, img: './img/pizza3.jpg' },
+  { name: 'Tropicano', kkal: 35, price: 18, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'tomatoSouse4' }, img: './img/pizza4.jpg' },
+  { name: '4sizon', price: 25, kkal: 390, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'tomatoSouse6' }, img: './img/pizza1.jpg' },
+  { name: '4meats', price: 15, kkal: 390, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'chilli' }, img: './img/pizza2.jpg' },
+  { name: 'Peperoni', price: 10, kkal: 390, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'garlick' }, img: './img/pizza3.jpg' },
+  { name: 'LaGalina', kkal: 40, price: 18, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'sweet' }, img: './img/pizza4.jpg' },
   { name: 'LaGalina with pineapple', price: 20, kkal: 500, list: { cheese: 'dor-blue', meet: 'peperoni', vegetables: 'sweet_garlick', souse: 'tomato' }, img: './img/pizza1.jpg' },
-  { name: '4Mashrooms', price: 28, kkal: 400, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'sweet' }, img: './img/pizza2.jpg' },
-  { name: 'Vegetarian', price: 54, kkal: 400, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'garlick' }, img: './img/pizza3.jpg' },
-  { name: 'otherPizza', kkal: 500, price: 16, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'another' }, img: './img/pizza4.jpg' }
+  { name: '4Mashrooms', price: 30, kkal: 400, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'sweet' }, img: './img/pizza2.jpg' },
+  { name: 'Vegetarian', price: 50, kkal: 400, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'garlick' }, img: './img/pizza3.jpg' },
+  { name: 'otherPizza', kkal: 500, price: 10, list: { cheese: 'dorBlue', meet: 'peperoni', vegetables: 'tomato', souse: 'another' }, img: './img/pizza4.jpg' }
 ];
-const components = { dorBlue: 100, peperoni: 200, tomato: 50, tomatoSouse: 40, tomatoSouse1: 10, tomatoSouse2: 15, tomatoSouse4: 20, garlick: 10, sweet: 5, another: 16, chilli: 5, sweet_garlick: 15 };
+const components = { dorBlue: 100, ham: 100, peperoni: 200, tomato: 50, tomatoSouse: 40, tomatoSouse1: 10, tomatoSouse2: 15, tomatoSouse4: 20, garlick: 10, sweet: 5, another: 16, chilli: 5, sweet_garlick: 15 };
+const cost = { dorBlue: 3, parmezan: 4, Mozzarella: 3, peperoni: 5, ham: 5, tomato: 2, tomatoSouse: 2, tomatoSouse1: 3, tomatoSouse2: 3, tomatoSouse4: 2, garlick: 1, sweet: 2, another: 4, chilli: 5, sweet_garlick: 5 };
 
 
 
 $("select")
 
   .change(function () {
-
-    if ($(this).val() == 'Table') {
-      console.log('table');
+    if ($(this).val() == 'None') {
+      $('.app').css('display', 'none');
+    }
+    else if ($(this).val() == 'Table') {
+      $('.app').css('display', 'block');
       $('.table').css({ 'display': 'flex', 'flex-direction': 'row' });
       $('.card-img').css('display', 'flex');
       $('.card').css({ 'display': 'flex', 'flex-direction': 'column', 'width': '30%' });
@@ -29,12 +32,12 @@ $("select")
       $('button').css('display', 'block');
     }
     else {
-      console.log('list');
+      $('.app').css('display', 'block');
       $('.table').css({ 'display': 'flex', 'flex-direction': 'column' });
       $('.card').css({ 'display': 'flex', 'width': '90%', 'flex-direction': 'row' });
       $('.card-img').css('display', 'none');
       $('.card-list').css('display', 'none');
-      $('button').css('display', 'none');
+      $('button #list').css('display', 'none');
     }
   })
   .change();
@@ -58,10 +61,8 @@ function createCardTable(pizza) {
 
 $(document).on('click', '#list', function (event) {
   var name = $(event.target).attr('class');
-  console.log(name);
   for (var key in pizzas) {
     if (pizzas[key].name == name) {
-      console.log(pizzas[key].list);
       window.kkal = pizzas[key].kkal;
       var templist = Object.entries(pizzas[key].list).toString().split(',');
       var list = [];
@@ -85,20 +86,19 @@ $(document).on('change', '.radio', function (event) {
     var component = $(this).attr('id');
     for (var key in components) {
       if (component === key) {
-        console.log(components[key]);
-        console.log(newKkal);
-        var newKkal = kkal + components[key] - components[key];
-        $('.newKkal').html('<div class="newKkal">Итого каллорий: ' + newKkal + ' kkal </div>');
+        newKkal = kkal + components[key];
+        kkal = newKkal;
+        $('.newKkal').html('<div class="newKkal">Итого каллорий: ' + newKkal + ' kkal </div> ');
       }
     }
   }
+
   else {
     var component = $(this).attr('id');
     for (var key in components) {
       if (component === key) {
-        console.log(components[key]);
         newKkal = kkal - components[key];
-        console.log(newKkal);
+        kkal = newKkal;
         $('.newKkal').html('<div class="newKkal">Итого каллорий: ' + newKkal + ' kkal </div> ');
       }
     }
